34c1a655bad6748d7361c2ca9ceb598c878ef8ca
Kevin Cyriac <149221177+kevincyriac-2005@users.noreply.github.com>
2026-08-17 22:59:38 +0530

    Update config.js

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,5 +1,5 @@
 const config = {
-    API_BASE_URL: 'https://civicpulse-backend-2uw5.onrender.com',
+    API_BASE_URL: 'https://civicpulse-backend-2uw5.onrender.com/api',
 };
 
 export default config;
